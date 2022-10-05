5e678097087a19ebf86aed36807da241b922d432
Юля <potemkin.vg@yandex.ru>
2022-10-05 22:16:42 +0300

    ДЗ 3. Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        // Задание 1
-        System.out.println("Задание 1");
-        int yearsOld = 17;
-        if (yearsOld >= 2 && yearsOld <= 6) {
-            System.out.println("Если возраст человека равен " + yearsOld + " то ему нужно ходить в детский сад");
-        } else if (yearsOld >= 7 && yearsOld <= 18) {
-            System.out.println("Если возраст человека равен " + yearsOld + " то ему нужно ходить в школу");
-        } else if (yearsOld > 18 && yearsOld <= 24) {
-            System.out.println("Если возраст человека равен " + yearsOld + " то ему нужно ходить в университет");
-        } else if (yearsOld > 24)
-            System.out.println("Если возраст человека равен " + yearsOld + " то ему нужно ходить на работу");
-    }}
+        // Задание 2
+        System.out.println("Задание 2");
+        int ageschildren = 14;
+        if (ageschildren < 5) {
+            System.out.println("Ребенок не может кататься на аттракционе");
+        } else if (ageschildren >= 5 && ageschildren < 14) {
+            System.out.println("Ребенок может кататься только в сопровождении взрослого");
+        } else if (ageschildren >= 14) {
+            System.out.println("Ребенок может кататься без сопровождения взрослого");
+        }}
+    {
+    }
+}
+
+
+
